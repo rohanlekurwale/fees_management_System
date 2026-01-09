a310c2e2814f32d4b0a4f1e15eb913621e48997f
rohanlekurwale <rohanlekurwale7274@gmail.com>
2026-01-10 00:06:25 +0530

    Manages the fee structure

---
+++ src/main/java/com/mycompany/fees_management_system/Fees_management_system.java
@@ -0,0 +1,10 @@
+package com.mycompany.fees_management_system;
+
+public class Fees_management_system {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new Login().setVisible(true); // तुमचा JFrame
+        });
+    }
+}
